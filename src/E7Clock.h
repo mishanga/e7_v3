44bff2deeaed7f361b973934d06d865d7d5e9670
Mikhail Troshev <mishanga@yandex-team.ru>
2025-09-02 13:30:49 +0300

    get rid of lcd

--- src/E7Clock.h
+++ src/E7Clock.h
@@ -17,9 +17,6 @@
 #define BTN_PLUS_PIN A2
 #define BTN_MINUS_PIN A3
 
-#define E7_LCD_COLS 16
-#define E7_LCD_ROWS 2
-
 #define FULL_DATE_FORMAT "DD.MM.YY hh:mm"
 #define FULL_DATE_FORMAT_NO_DOT "DD.MM.YY hh mm"
 
@@ -32,7 +29,6 @@ class E7Clock {
 
 private:
   RTC_DS3231 _rtc;
-  LiquidCrystal_I2C _lcd;
   uint16_t _prd;
   uint32_t _tmr;
   uint32_t _tmr_set;
@@ -44,39 +40,15 @@ private:
   Button _btnPlus;
   DateTime _now;
 
-  void _clearLcd() {
-    _lcd.setCursor(0, 1);
-    _lcd.print("                ");
-  }
-
   void _updateView() {
     if (_mode == E7_SHOW) {
-      _lcd.noBacklight();
-      _lcd.setCursor(1, 0);
-
-      char format[] = FULL_DATE_FORMAT;
-      char format_no_dot[] = FULL_DATE_FORMAT_NO_DOT;
-      _lcd.print(_now.toString((_now.second() % 2) ? format : format_no_dot));
-
-      String sTemp = getTempStr();
-      _lcd.setCursor(E7_LCD_COLS - sTemp.length() - 1, 1);
-      _lcd.print(sTemp);
-
-      _lcd.setCursor(0, 1);
     }
 
     if (_mode == E7_SET) {
-      _lcd.backlight();
-      _lcd.setCursor(1, 0);
-      _lcd.print(getFullDateTime());
-
-      _lcd.setCursor(1 + (4 - _state) * 3, 1);
-      _lcd.print("^^");
     }
   }
 
   void _resetView() {
-    _clearLcd();
     _updateView();
   }
 
@@ -133,8 +105,7 @@ private:
 
 public:
   E7Clock(uint16_t set_delay = 10)
-    : _lcd(0x27, E7_LCD_COLS, E7_LCD_ROWS),
-      _prd(1000),
+    : _prd(1000),
       _tmr(0),
       _tmr_set(0),
       _mode(E7_SHOW),
@@ -146,7 +117,6 @@ public:
 
   void begin() {
     _rtc.begin();
-    _lcd.init();
     _now = _rtc.now();
   }
 
@@ -169,18 +139,6 @@ public:
 
   DateTime now() {
     return _now;
-  }
-
-  String getTempStr() {
-    float fTemp = _rtc.getTemperature();
-    String sTemp(fTemp, 1);
-
-    if (fTemp > 0) {
-      sTemp = "+" + sTemp;
-    }
-    sTemp.concat("\xDF");
-    sTemp.concat("C");  // °C
-    return sTemp;
   }
 
   int8_t getTempInt() {
